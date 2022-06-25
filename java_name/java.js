4d22ca3ce29b9671a7d6916ce4864156803a4fc3
Titi02 <82676982+Titi02@users.noreply.github.com>
2022-06-25 14:48:13 +0100

    adding files

---
+++ java_name/java.js
@@ -0,0 +1,11 @@
+const _name = "Name: Osho Muyibat Titilope";
+const height = "My height is: 6.1ft";
+const country = "Country: Nigeria";
+
+document.write(_name + '<br>');
+document.write(height + '<br>');
+document.write(country + '<br>');
+
+alert(_name+ '\n' + height +'+\n' + country)
+
+console.log(_name+ '\n' + height +'\n' + country)
